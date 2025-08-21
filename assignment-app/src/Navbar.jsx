6fc00d5e33959ea670314568056bfc776a53fb2b
Alvin Omnes <omnes517@gmail.com>
2025-08-20 17:28:35 -0700

    working navbar.jsx

---
+++ assignment-app/src/Navbar.jsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom";
+
+function NavBar () {
+    return (
+        <nav className="navbar navbar-dark bg-dark fixed-top">
+            <div className="container-fluid">
+
+            </div>
+        </nav>
+    )
+}
