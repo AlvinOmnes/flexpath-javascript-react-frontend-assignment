6d1c45dc333c3bbad979c8672cd6aa3bbd1c0d7b
Alvin Omnes <omnes517@gmail.com>
2025-08-21 19:41:59 -0700

    updated navBar.jsx

--- assignment-app/src/Navbar.jsx
+++ assignment-app/src/Navbar.jsx
@@ -1,10 +1,11 @@
 import {Link} from "react-router-dom";
 
-function NavBar () {
+function navBar () {
     return (
         <nav className="navbar navbar-dark bg-dark fixed-top">
             <div className="container-fluid">
-
+            <Link className="nav-link" to="/">User Behaviour Data</Link>
+            <Link className="nav-link" to="/search">Search Through Dataset</Link>
             </div>
         </nav>
     )
