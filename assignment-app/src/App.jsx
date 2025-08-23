f7aaba271f94c18ce0b2564a72b956b6de6414c5
Alvin Omnes <omnes517@gmail.com>
2025-08-23 15:59:55 -0700

    update navBar

--- assignment-app/src/App.jsx
+++ assignment-app/src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-//import navBar from "./navBar.jsx"
+import NavBar from "./navBar.jsx"
 import DataInfo from "./dataInfo.jsx"
 
 function App() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        {/* <p> tag is a placeholder, You'll need to change the tag/component type later*/}
+      <div>
+        <NavBar />
+      </div>
+      {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        { <p> tag is a placeholder, You'll need to change the tag/component type later}
         <p className="navbar-brand ms-4 nav-link">User Behavior Data</p>
-      </nav>
+      </nav>*/}
       <hr />
       <div>
         <DataInfo />
--- assignment-app/src/Navbar.jsx
+++ assignment-app/src/Navbar.jsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
+import React from "react"
 
-export function navBar() {
+export function NavBar() {
     return (
-        <nav className="navbar navbar-dark bg-dark fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
             <div className="container-fluid">
                 <div className="navbar-nav">
                     <Link className="nav-link" to="/">User Behaviour Data</Link>
